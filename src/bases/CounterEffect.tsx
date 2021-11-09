302d7873c0ec635997e2f1f449a5d40ed1e282a1
= <=>
2021-11-09 13:54:33 +0100

    UseRef. gsap TimeLines.

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 
 const MAXIMUN_COUNT = 10;
@@ -6,6 +6,8 @@ const MAXIMUN_COUNT = 10;
 export const CounterEffect = () => {
 
     const [counter, setCounter] = useState( 5);
+
+    const counterElement = useRef<HTMLHeadingElement>( null );
 
     const handleClick = ( numero: number ) => {
             setCounter( prev => Math.min( prev + numero, MAXIMUN_COUNT ) );
@@ -17,17 +19,22 @@ export const CounterEffect = () => {
         
         console.log('%cValor máximo alcanzado', 'color: red; background-color: black');
 
-        gsap.to( 'h2', { y: -10, duration: 0.2, ease: 'ease.out' } )
-            .then( () => {
-                gsap.to( 'h2', { y: 0, duration: 1, ease: 'bounce.out' } )
-            });
+        const tl = gsap.timeline();
+
+        tl.to( counterElement.current, { y: -10, duration: 0.2, ease: 'ease.out' } )
+            .to( counterElement.current, { y: 0, duration: 1, ease: 'bounce.out' } );
+
+        // gsap.to( counterElement.current, { y: -10, duration: 0.2, ease: 'ease.out' } )
+        //     .then( () => {
+        //         gsap.to( counterElement.current, { y: 0, duration: 1, ease: 'bounce.out' } )
+        //     });
 
     }, [counter])
 
     return (
         <>
             <h1>Counter Effect: </h1>
-            <h2>{ counter }</h2>
+            <h2 ref={ counterElement }>{ counter }</h2>
 
             <button onClick={ () => handleClick(1) }>
                 +1
