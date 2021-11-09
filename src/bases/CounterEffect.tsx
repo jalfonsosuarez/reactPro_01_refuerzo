d947593e00d6175189ad3350390263b237bc43c4
= <=>
2021-11-09 13:38:58 +0100

    Gsap animations.

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
 
 const MAXIMUN_COUNT = 10;
 
@@ -16,11 +17,17 @@ export const CounterEffect = () => {
         
         console.log('%cValor máximo alcanzado', 'color: red; background-color: black');
 
+        gsap.to( 'h2', { y: -10, duration: 0.2, ease: 'ease.out' } )
+            .then( () => {
+                gsap.to( 'h2', { y: 0, duration: 1, ease: 'bounce.out' } )
+            });
+
     }, [counter])
 
     return (
         <>
-            <h1>Counter Effect: { counter }</h1>
+            <h1>Counter Effect: </h1>
+            <h2>{ counter }</h2>
 
             <button onClick={ () => handleClick(1) }>
                 +1
